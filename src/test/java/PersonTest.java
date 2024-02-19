b59b1f94019b62350f7ac176c30067c44dc0196a
unknown <boumedienekadaben@gmail.com>
2024-02-19 09:32:17 -0700

    Test de la methode isAdult

--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class PersonTest {
+import org.example.Person;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class PersonTest {
+    Person p;
+
+    @BeforeEach
+    public void setup() {
+       p = new Person("Boumediene","Kadaben",17);
+    }
+
+
+    @Test
+    public void getFullNameTest(){
+
+        String FullNameResult = p.getFullName();
+
+        Assertions.assertEquals("Boumediene Kadaben", FullNameResult);
+    }
+
+    @Test
+    public void isAdult(){
+        boolean AgeResult = p.isAdult();
+        Assertions.assertEquals(true, AgeResult);
+    }
+
+    @Test
+    public void isNotAdult(){
+        boolean AgeResult = p.isAdult();
+        Assertions.assertEquals(false, AgeResult);
+    }
 }
